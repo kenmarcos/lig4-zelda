1b78ee227d6dbafd8406262beba5f4cb8312813b
maykel_kenzie <maykelnekel@gmail.com>
2021-06-10 20:46:24 -0300

    projeto fializado, identado, revisado e delicinha

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ let table = [
     ["X", "X", "X", "X", "X", "X", "X"],
     ["X", "X", "X", "X", "X", "X", "X"],
     ["X", "X", "X", "X", "X", "X", "X"],
-
 ];
 
 let player1
-let player2 
-
+let player2
 let win
+let turn = 'turn1'
+let player1winCount = 0
+let player2WinCount = 0
 
-let game = document.querySelector("#game");
-let timerContainer = document.getElementById('timerContainer')
-
+const game = document.querySelector("#game");
+const timerContainer = document.getElementById('timerContainer')
+const columns = document.querySelectorAll('.column')
 const btnQuit = document.getElementById("quit");
+const divPlayer = document.getElementById('player')
+const btnRestart = document.getElementById("restart");
+const showInfos = document.getElementById('showInfos')
+const buttons = document.getElementsByClassName('buttons')[0]
 
 //FUNCAO PARA CRIAR TABALE COM BASE NA ARRAY TABLE   
 const createTable = () => {
@@ -26,26 +31,26 @@ const createTable = () => {
 
     document.getElementById('game').innerHTML = '';
     document.getElementById('timerContainer').innerHTML = '';
-        for (let i = 0; i < 7; i++) {
-            let column = document.createElement("div");
-            column.setAttribute('id', i)
-            column.classList.add("column");
-            game.append(column);
-            column.addEventListener('click', changeTurn)
-            for (let j = 0; j < table.length; j++) {
-                let cell = document.createElement("div");
-                cell.classList.add("cell");
-                column.append(cell);
-
-            }
+    for (let i = 0; i < 7; i++) {
+        let column = document.createElement("div");
+        column.setAttribute('id', i)
+        column.classList.add("column");
+        game.append(column);
+        column.addEventListener('click', changeTurn)
+        for (let j = 0; j < table.length; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            column.append(cell);
         }
-        const timer = document.createElement('div')
-        timer.setAttribute('id','timer')
-        setTimeout(function() {
-        return timerContainer.appendChild(timer)}, 1000);
-        player1Turn()
-        playCounter(0)
     }
+    const timer = document.createElement('div')
+    timer.setAttribute('id', 'timer')
+    setTimeout(function () {
+        return timerContainer.appendChild(timer)
+    }, 1000);
+    player1Turn()
+    playCounter(0)
+}
 
 //funcao reiniciar table
 const restartTable = () => {
@@ -55,17 +60,17 @@ const restartTable = () => {
         for (let j = 0; j < table[i].length; j++) {
             table[i][j] = "X";
         }
-    }    
+    }
 }
 let timerID
 function timer() {
     let sec = 0;
     let min = 0;
     let hr = 0;
-    timerID = setInterval(function() {
+    timerID = setInterval(function () {
         let timer = (hr < 10 ? '0' + hr : hr) + ':' + (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec);
         document.getElementById("timer").innerHTML = timer;
-       
+
         if (sec === 59) {
             min++;
             sec = 0;
@@ -77,14 +82,12 @@ function timer() {
         sec++
     }, 1000);
 }
-
 const stopTimer = () => {
     clearInterval(timerID);
 }
 
 //chamada mensagem de vitoria
 const finalMsg = (player) => {
-  
     if (player === 'alertEmpate') {
         const alert = document.createElement('div')
         alert.classList.add(player)
@@ -95,7 +98,7 @@ const finalMsg = (player) => {
         image.setAttribute('class', `${player}-img`)
         alert.appendChild(image)
         game.appendChild(alert)
-       
+
     } else if (player === 'alertPlayer1') {
         const alert = document.createElement('div')
         alert.classList.add(player)
@@ -106,7 +109,7 @@ const finalMsg = (player) => {
         image.setAttribute('class', `${player}-img`)
         alert.appendChild(image)
         game.appendChild(alert)
-       
+
     } else if (player === 'alertPlayer2') {
         const alert = document.createElement('div')
         alert.classList.add(player)
@@ -117,7 +120,6 @@ const finalMsg = (player) => {
         image.setAttribute('class', `${player}-img`)
         alert.appendChild(image)
         game.appendChild(alert)
-     
     }
     timerAfterVictory();
 }
@@ -183,7 +185,7 @@ const verticalVictroy = (arr) => {
                     stopTimer();
                     playCounter(2)
                     win = player2
-                   break
+                    break
                 }
             }
         }
@@ -241,24 +243,17 @@ const draw = (arr) => {
 }
 
 const registerPosition = (id, player) => {
-
     for (let i = 5; i >= 0; i--)
         if (table[i][id] === "X") {
             table[i][id] = player
             break;
         }
-    console.log(table);
 }
-
-const columns = document.querySelectorAll('.column')
-let turn = 'turn1'
 
 // FUNÇÃO DE MUDANÇA DE TURNO E COLOCAÇÃO DOS DISCOS
 const changeTurn = (evt) => {
     // selecionar a coluna
-
     let selectedColumn = evt.currentTarget
-
     // selecionar última célula vazia da coluna
     let celula
     for (let i = 0; i < selectedColumn.childNodes.length; i++) {
@@ -266,7 +261,6 @@ const changeTurn = (evt) => {
             celula = selectedColumn.childNodes[i]
         }
     }
-        
     if (turn === 'turn1') { // turno do jogador 1
         // colocar o disco do jogador 1
         const disc1 = document.createElement('div')
@@ -283,7 +277,7 @@ const changeTurn = (evt) => {
         }
     } else { // turno do jogador 2
         // colocar o disco do jogador 2
-        
+
         const disc2 = document.createElement('div')
         disc2.classList.add('disc2')
         celula.appendChild(disc2)
@@ -296,14 +290,14 @@ const changeTurn = (evt) => {
         if (win === undefined) {
             player1Turn()
         }
-    } 
+    }
 }
 
 function timer() {
     let sec = 0;
     let min = 0;
     let hr = 0;
-    timerID = setInterval(function() {
+    timerID = setInterval(function () {
         let timer = (hr < 10 ? '0' + hr : hr) + ':' + (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec);
         document.getElementById("timer").innerHTML = timer;
         if (sec === 59) {
@@ -318,11 +312,6 @@ function timer() {
     }, 1000);
 }
 
-// const stopTimer = () => {
-//     clearInterval(timerID);
-// }
-
-
 const toStart = () => {
     let inputsNames = document.querySelectorAll('.input')
     if (inputsNames[0].value === "" || inputsNames[1].value === "") {
@@ -332,16 +321,16 @@ const toStart = () => {
         player1 = inputsNames[0].value
         player2 = inputsNames[1].value
     }
-    turn = 'turn1' 
+    turn = 'turn1'
     restartTable();
     createTable();
-    stopTimer()    
+    stopTimer()
     timer();
     win = undefined
 }
 
 const toRestar = () => {
-    turn = 'turn1' 
+    turn = 'turn1'
     restartTable();
     createTable();
     stopTimer()
@@ -349,123 +338,113 @@ const toRestar = () => {
     win = undefined
 }
 
-const btnRestart = document.getElementById("restart");
-btnRestart.addEventListener('click', toRestar);
-
 const getNames = () => {
+    const form = document.createElement('form')
+    form.classList.add('form')
+    game.append(form)
 
+    const divInput1 = document.createElement('div')
+    divInput1.classList.add('div-input')
+    form.appendChild(divInput1)
+    const imgPlayer1 = document.createElement('img')
+    imgPlayer1.src = './img/link.png'
+    imgPlayer1.classList.add('img1')
+    divInput1.appendChild(imgPlayer1)
+    const inputPlayer1 = document.createElement('input')
+    inputPlayer1.setAttribute('type', 'text')
+    inputPlayer1.setAttribute('placeholder', 'Player 1')
+    inputPlayer1.classList.add('input')
+    divInput1.appendChild(inputPlayer1)
 
-  const form = document.createElement('form')
-  form.classList.add('form')
-  game.append(form)
+    const versus = document.createElement('p')
+    versus.classList.add('versus')
+    versus.innerText = 'VS'
+    form.appendChild(versus)
 
-  const divInput1 = document.createElement('div')
-  divInput1.classList.add('div-input')
-  form.appendChild(divInput1)
-  const imgPlayer1 = document.createElement('img')
-  imgPlayer1.src = './img/link.png'
-  imgPlayer1.classList.add('img1')
-  divInput1.appendChild(imgPlayer1)
-  const inputPlayer1 = document.createElement('input')
-  inputPlayer1.setAttribute('type', 'text')
-  inputPlayer1.setAttribute('placeholder', 'Player 1')
-  inputPlayer1.classList.add('input')
-  divInput1.appendChild(inputPlayer1)
+    const divInput2 = document.createElement('div')
+    divInput2.classList.add('div-input')
+    form.appendChild(divInput2)
+    const imgPlayer2 = document.createElement('img')
+    imgPlayer2.src = './img/vilain.png'
+    imgPlayer2.classList.add('img2')
+    divInput2.appendChild(imgPlayer2)
+    const inputPlayer2 = document.createElement('input')
+    inputPlayer2.setAttribute('type', 'text')
+    inputPlayer2.setAttribute('placeholder', 'Player 2')
+    inputPlayer2.classList.add('input')
+    divInput2.appendChild(inputPlayer2)
 
-  const versus = document.createElement('p')
-  versus.classList.add('versus')
-  versus.innerText = 'VS'
-  form.appendChild(versus)
-
-  const divInput2 = document.createElement('div')
-  divInput2.classList.add('div-input')
-  form.appendChild(divInput2)
-  const imgPlayer2 = document.createElement('img')
-  imgPlayer2.src = './img/vilain.png'
-  imgPlayer2.classList.add('img2')
-  divInput2.appendChild(imgPlayer2)
-  const inputPlayer2 = document.createElement('input')
-  inputPlayer2.setAttribute('type', 'text')
-  inputPlayer2.setAttribute('placeholder', 'Player 2')
-  inputPlayer2.classList.add('input')
-  divInput2.appendChild(inputPlayer2)
-
-  const btnStart = document.createElement('button')
-  btnStart.innerText = 'Start Game'
-  btnStart.classList.add("btnStart");
-  form.appendChild(btnStart)
-  btnStart.addEventListener('click', toStart);
+    const btnStart = document.createElement('button')
+    btnStart.innerText = 'Start Game'
+    btnStart.classList.add("btnStart");
+    form.appendChild(btnStart)
+    btnStart.addEventListener('click', toStart);
 }
 getNames()
 
-const divPlayer = document.getElementById('player')
+
 
 const player1Turn = () => {
-  divPlayer.innerHTML = " "
-  divPlayer.classList.remove('div-player2')
-  divPlayer.classList.add('div-player1')
+    divPlayer.innerHTML = " "
+    divPlayer.classList.remove('div-player2')
+    divPlayer.classList.add('div-player1')
 
-  const pPlayer1 = document.createElement('p')
-  pPlayer1.innerText = player1
-  pPlayer1.classList.add('p-player1')
-  divPlayer.appendChild(pPlayer1)
+    const pPlayer1 = document.createElement('p')
+    pPlayer1.innerText = player1
+    pPlayer1.classList.add('p-player1')
+    divPlayer.appendChild(pPlayer1)
 
-  const imgPlayer1 = document.createElement('img')
-  imgPlayer1.src = './img/link.png'
-  imgPlayer1.classList.add('img-div')
-  divPlayer.appendChild(imgPlayer1)
+    const imgPlayer1 = document.createElement('img')
+    imgPlayer1.src = './img/link.png'
+    imgPlayer1.classList.add('img-div')
+    divPlayer.appendChild(imgPlayer1)
 }
 
 const player2Turn = () => {
-  divPlayer.innerHTML = " "
-  divPlayer.classList.remove('div-player1')
-  divPlayer.classList.add('div-player2')
+    divPlayer.innerHTML = " "
+    divPlayer.classList.remove('div-player1')
+    divPlayer.classList.add('div-player2')
 
-  const pPlayer2 = document.createElement('p')
-  pPlayer2.innerText = player2
-  pPlayer2.classList.add('p-player2')
-  divPlayer.appendChild(pPlayer2)
+    const pPlayer2 = document.createElement('p')
+    pPlayer2.innerText = player2
+    pPlayer2.classList.add('p-player2')
+    divPlayer.appendChild(pPlayer2)
 
-  const imgPlayer2 = document.createElement('img')
-  imgPlayer2.src = './img/vilain.png'
-  imgPlayer2.classList.add('img-div')
-  divPlayer.appendChild(imgPlayer2)
-
+    const imgPlayer2 = document.createElement('img')
+    imgPlayer2.src = './img/vilain.png'
+    imgPlayer2.classList.add('img-div')
+    divPlayer.appendChild(imgPlayer2)
 }
 
-let player1winCount = 0
-let player2WinCount = 0
+
 const playCounter = (player) => {
-    
-    if(player === 1){
+    if (player === 1) {
         player1winCount++
-    }else if(player === 2){
+    } else if (player === 2) {
         player2WinCount++
     }
-  
+
     let containerP1 = document.querySelector('#player1count');
     let containerP2 = document.querySelector('#player2count');
     let countPlayer1 = document.createElement('div');
     countPlayer1.classList.add('playCount');
     containerP1.innerHTML = ""
-    countPlayer1.innerHTML = player1 + " : " + player1winCount;
+    countPlayer1.innerHTML = player1 + " " + player1winCount;
     let countPlayer2 = document.createElement('div');
     countPlayer2.classList.add('playCount');
     containerP2.innerHTML = ""
-    countPlayer2.innerHTML = player2 + " : " + player2WinCount;
+    countPlayer2.innerHTML = player2 + " " + player2WinCount;
     containerP1.append(countPlayer1)
     containerP2.append(countPlayer2)
 };
-
-const showInfos = document.getElementById('showInfos')
-const buttons = document.getElementsByClassName('buttons')[0]
 
 const toQuit = () => {
     document.location.reload(true)
 }
 
-btnQuit.addEventListener('click', toQuit);
-
 const timerAfterVictory = () => {
     setTimeout(toRestar, 4000);
 }
+
+btnRestart.addEventListener('click', toRestar);
+btnQuit.addEventListener('click', toQuit);
